19cdd15914f377556687f8f4ed318e1641ed2813
Viktor <vikabr0524@gmail.com>
2026-01-30 11:45:35 +0100

    added lab1 files

--- te.c
+++ te.c
@@ -1,1 +1,14 @@
-avada kadabra
+#include <stdint.h>
+#include <stdio.h>
+#include "kernel_functions.h"
+
+enum exception init_kernal(void);
+enum exception create_task(void);
+void run(void);
+void terminate(void);
+
+int main()
+{
+
+  return 0;
+}
