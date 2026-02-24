0f10b646c18d6cd1fe858a00dfb166cf20a63311
Viktor Abrahamsson <vikabr0524@gmail.com>
2026-02-24 14:37:32 +0100

    sync

--- src/concurrency.c
+++ src/concurrency.c
@@ -18,7 +18,29 @@ void turn_off_led(int index)
 }
 void flash_led(int index)
 {
+  for (int i = 0; i < 8000; i++)
+  {
+    for (int j = 0; j < 100; j++)
+    {
+      ;
+    }
+  }
+  turn_off_led(index);
 }
 void compute_primes(void)
 {
+  volatile long long x, y, n = 4000;
+  bool isprime;
+  for (x = 2; x < n; x++)
+  {
+    isprime = 1;
+    for (y = 2; y <= x; y++)
+    {
+      if ((x % y) == 0)
+      {
+        isprime = 0;
+        break;
+      }
+    }
+  }
 }
