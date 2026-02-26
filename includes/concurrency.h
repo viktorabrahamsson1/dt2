f88188f9b0490f4a75a8e85dc8849318ea9aafc4
Viktor Abrahamsson <vikabr0524@gmail.com>
2026-02-26 13:42:25 +0100

    lab 3, first main

--- includes/concurrency.h
+++ includes/concurrency.h
@@ -8,12 +8,17 @@ uint task_2_deadline;
 uint task_3_deadline;
 uint task_4_deadline;
 
+// Auxiliary funktioner
 void setup(void);
 void turn_on_led(int index);
 void turn_off_led(int index);
 void flash_led(int index);
 void compute_primes(void);
 
-
+// Task funktioner
+void task_1(void);
+void task_2(void);
+void task_3(void);
+void task_4(void);
 
 #endif
--- src/concurrency.c
+++ src/concurrency.c
@@ -1,20 +1,54 @@
 
+#include "../drivers/at91sam3x8.h"
+#include "../drivers/system_sam3x.h"
 #include "../includes/concurrency.h"
 #include "../includes/kernel_functions.h"
 
-uint task_1_deadline = 1000;
-uint task_2_deadline = 1000;
-uint task_3_deadline = 1000;
-uint task_4_deadline = 1000;
+uint task_1_deadline = 10000;
+uint task_2_deadline = 10000;
+uint task_3_deadline = 10000;
+uint task_4_deadline = 10000;
 
 void setup(void)
 {
+  *AT91C_PMC_PCER |= (1 << 14) | (1 << 11) | (1 << 13);
+
+  // FÖR LEDS
+  *AT91C_PIOC_PER = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+  *AT91C_PIOC_PPUDR = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+  ;
+  *AT91C_PIOC_OER = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+  ;
+
+  // FÖR KNAPPAR
+  // Knapp 1:
+  *AT91C_PIOA_PER = (1 << 14);
+  *AT91C_PIOA_PPUER = (1 << 14);
+  *AT91C_PIOA_ODR = (1 << 14);
+
+  // Knapp 2:
+  *AT91C_PIOD_PER = (1 << 0);
+  *AT91C_PIOD_PPUER = (1 << 0);
+  *AT91C_PIOD_ODR = (1 << 0);
+
+  // Sätt på interrupts för port A
+  *AT91C_PIOA_IER = (1 << 14);
+  NVIC_ClearPendingIRQ(PIOA_IRQn);
+  NVIC_EnableIRQ(PIOA_IRQn);
 }
 void turn_on_led(int index)
 {
+  if (index < 1 || index > 4)
+    return;
+
+  *AT91C_PIOD_SODR = (1 << index);
 }
 void turn_off_led(int index)
 {
+  if (index < 1 || index > 4)
+    return;
+
+  *AT91C_PIOD_CODR = (1 << index);
 }
 void flash_led(int index)
 {
@@ -43,4 +77,92 @@ void compute_primes(void)
       }
     }
   }
+}
+
+// Task funktioner
+
+void task_1(void)
+{
+  while (1)
+  {
+    turn_on_led(1);
+    compute_primes();
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+      flash_led(1);
+      exception r = wait(8000);
+      set_deadline(task_1_deadline + ticks());
+    }
+  }
+}
+
+void task_2(void)
+{
+  while (1)
+  {
+    turn_on_led(2);
+    compute_primes();
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+      flash_led(2);
+      exception r = wait(8000);
+      set_deadline(task_2_deadline + ticks());
+    }
+  }
+}
+
+void task_3(void)
+{
+  while (1)
+  {
+    turn_on_led(3);
+    compute_primes();
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+      flash_led(3);
+      exception r = wait(8000);
+      set_deadline(task_3_deadline + ticks());
+    }
+  }
+}
+
+void task_4(void)
+{
+  while (1)
+  {
+    // kollar om button 1 trycks
+    if ((*AT91C_PIOD_PDSR & (1 << 0)) == 0)
+      while ((*AT91C_PIOD_PDSR & (1 << 0)) == 0)
+      {
+        flash_led(4);
+      }
+    turn_off_led(4);
+  }
+
+  exception r = wait(10);
+  (void)r;
+
+  set_deadline(task_4_deadline + ticks());
+}
+
+// MAIN
+
+int main(void)
+{
+
+  setup();
+  SysTick_Config(83999);
+
+  create_task(task_1, task_1_deadline);
+  create_task(task_2, task_2_deadline);
+  create_task(task_3, task_3_deadline);
+  create_task(task_4, task_4_deadline);
+
+  while (1)
+  {
+  }
+  return 0;
 }
